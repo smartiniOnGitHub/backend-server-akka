d3d71d6df49949335e486e9b861c5e8333658de3
Sandro Martini <sandro.martini@gmail.com>
2013-04-05 20:47:20 +0200

    update sbt to 0.12.3, scala to 2.10.1, akka to 2.1.2

--- project/BackendServerAkkaBuild.scala
+++ project/BackendServerAkkaBuild.scala
@@ -6,10 +6,10 @@ object BackendServerAkkaBuild extends Build
 	val Organization = "backend_server_akka"
 	val ArtifactName = "backend-server-akka"
 	val Version      = "0.1"
-	val ScalaVersion = "2.9.2"  // "2.10.0"
-	val AkkaVersion  = "2.0.5"  // "2.1.0"
+	val ScalaVersion = "2.10.1"
+	val AkkaVersion  = "2.1.2"
 
-// TODO: set requirements to akka-2.1.0 and scala-2.10.0, and add all the rest inside this build file ...
+// TODO: update other requirements for the chosen scala and akka versions ...
 // TODO: verify if set even Java 7 as requirement here ...
 
 }
