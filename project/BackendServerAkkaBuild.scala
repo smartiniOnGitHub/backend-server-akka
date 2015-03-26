0dee757e9d981d44003ccf5ca8b05df4be54fc14
Sandro Martini <sandro.martini@gmail.com>
2015-03-26 15:02:14 +0100

    update dependencies to latest (major) releases

--- project/BackendServerAkkaBuild.scala
+++ project/BackendServerAkkaBuild.scala
@@ -7,8 +7,8 @@ object BackendServerAkkaBuild extends Build
 	val ArtifactName = "backend-server-akka"
 	val MainPackage  = "bsa"  // the base package for this project, here just as info
 	val Version      = "0.1"
-	val ScalaVersion = "2.10.5"
-	val AkkaVersion  = "2.2.5"
+	val ScalaVersion = "2.11.6"
+	val AkkaVersion  = "2.3.9"
 
 // TODO: update other requirements for the chosen scala and akka versions ...
 // TODO: verify if set even Java 7 as requirement here ...
